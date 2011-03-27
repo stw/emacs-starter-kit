b24894364b0311182600514c27e9a8e275dc7b4d
Steve Walker <swalker@walkertek.com>
2011-03-27 14:11:57 -0400

    setup

--- init.el
+++ init.el
@@ -76,4 +76,30 @@
 (if (file-exists-p user-specific-dir)
   (mapc #'load (directory-files user-specific-dir nil ".*el$")))
 
+(color-theme-zenburn)
+(split-window-vertically)
+(slime)
+
 ;;; init.el ends here
+
+(defun rotate-windows ()
+ "Rotate your windows" (interactive) (cond ((not (> (count-windows) 1)) (message "You can't rotate a single window!"))
+(t
+ (setq i 1)
+ (setq numWindows (count-windows))
+ (while  (< i numWindows)
+   (let* (
+          (w1 (elt (window-list) i))
+          (w2 (elt (window-list) (+ (% i numWindows) 1)))
+
+          (b1 (window-buffer w1))
+          (b2 (window-buffer w2))
+
+          (s1 (window-start w1))
+          (s2 (window-start w2))
+          )
+     (set-window-buffer w1  b2)
+     (set-window-buffer w2 b1)
+     (set-window-start w1 s2)
+     (set-window-start w2 s1)
+     (setq i (1+ i)))))))
