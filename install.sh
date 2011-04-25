bde5afb97173d3c83c58c38d53e79b4957237671
Steve Walker <swalker@walkertek.com>
2011-04-25 09:28:51 -0400

    added install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+git submodule add git@github.com:stw/emacs-rails.git
+git submodule add https://github.com/emacsmirror/ecb.git
+git submodule add https://github.com/emacsmirror/org-mode.git 
