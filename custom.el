4dca07d8e6716269fdd0aca1b2d84e8b561602a5
Steve Walker <swalker@walkertek.com>
2011-04-25 09:14:20 -0400

    updated config

---
+++ custom.el
@@ -0,0 +1,32 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 211 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(default-frame-alist (quote ((font-backend ns) (fontsize . 0) (font . "-apple-Monaco-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1") (cursor-color . "Red") (left-fringe . 4) (right-fringe . 12) (menu-bar-lines . 1) (modeline . t) (vertical-scroll-bars) (fringe) (internal-border-width . 0) (cursor-type . box) (background-color . "#111111") (background-mode . dark) (border-color . "#111111") (foreground-color . "#dcdccc") (mouse-color . "#dcdccc") (tool-bar-lines . 0))))
+ '(display-time-mode t)
+ '(ecb-layout-name "right6")
+ '(ecb-options-version "2.40")
+ '(ecb-show-sources-in-directories-buffer (quote always))
+ '(ecb-source-path (quote (("/" "Root"))))
+ '(ecb-tip-of-the-day nil)
+ '(ecb-tree-indent 2)
+ '(ecb-vc-enable-support t)
+ '(ns-antialias-text nil)
+ '(ns-tool-bar-display-mode nil t)
+ '(ns-tool-bar-size-mode nil t)
+ '(org-agenda-files (quote ("~/org/business.org" "~/org/personal.org")))
+ '(tabbar-mode nil nil (tabbar))
+ '(visual-line-mode nil t)
+ '(weblogger-config-alist (quote (("stephenwalker.com" "http://www.stephenwalker.com/xmlrpc.php" "admin" "" "1")))))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(emacs-lisp-mode-default ((t (:inherit autoface-default :height 140 :family "Monaco"))) t)
+ '(org-mode-default ((t (:inherit outline-mode-default :stipple nil :strike-through nil :underline nil :slant normal :weight normal :height 140 :width normal :family "Monaco"))))
+ '(slime-repl-mode-default ((t (:inherit autoface-default :height 140 :family "Monaco"))) t))
