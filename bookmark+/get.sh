ef5e8fe14fee1c3d5dbdca0e14eb69d4c9d0b0a7
Steve Walker <swalker@walkertek.com>
2011-06-09 15:08:44 -0400

    added bookmark+

---
+++ bookmark+/get.sh
@@ -0,0 +1,12 @@
+#!/bin/sh 
+
+# http://www.emacswiki.org/emacs/download/bookmark%2b.el 
+
+wget http://www.emacswiki.org/emacs/download/bookmark%2b.el
+wget http://www.emacswiki.org/emacs/download/bookmark%2b-mac.el 
+wget http://www.emacswiki.org/emacs/download/bookmark%2b-bmu.el 
+wget http://www.emacswiki.org/emacs/download/bookmark%2b-1.el 
+wget http://www.emacswiki.org/emacs/download/bookmark%2b-key.el 
+wget wget http://www.emacswiki.org/emacs/download/bookmark%2b-lit.el 
+wget http://www.emacswiki.org/emacs/download/bookmark%2b-doc.el 
+wget http://www.emacswiki.org/emacs/download/bookmark%2b-chg.el 
