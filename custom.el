b1aeb5edf653926b5e4fe30264b730386833d72c
j1 <steve@j1.(none)>
2012-05-09 19:59:49 -0400

    updating submodules

--- custom.el
+++ custom.el
@@ -4,9 +4,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aquamacs-additional-fontsets nil t)
- '(aquamacs-customization-version-id 300 t)
- '(aquamacs-tool-bar-user-customization nil t)
  '(default-frame-alist (quote ((tool-bar-lines . 0) (cursor-type . box) (internal-border-width . 0) (fringe) (vertical-scroll-bars) (modeline . t) (menu-bar-lines . 1) (right-fringe . 12) (left-fringe . 4) (cursor-color . "Red") (font . "-apple-Monaco-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1") (fontsize . 0) (font-backend ns) (background-color . "#111111") (background-mode . dark) (border-color . "#111111") (foreground-color . "#f0f0f0") (mouse-color . "#f0f0f0"))))
  '(display-time-mode t)
  '(ecb-layout-name "right6")
@@ -19,7 +16,6 @@
  '(ns-antialias-text nil)
  '(ns-tool-bar-display-mode nil t)
  '(ns-tool-bar-size-mode nil t)
- '(one-buffer-one-frame-mode nil nil (aquamacs-frame-setup))
  '(org-agenda-files (quote ("~/org/business.org" "~/org/personal.org")))
  '(php-file-patterns nil)
  '(safe-local-variable-values (quote ((Syntax . Common-Lisp) (Mode . Lisp) (ruby-compilation-executable . "ruby") (ruby-compilation-executable . "ruby1.8") (ruby-compilation-executable . "ruby1.9") (ruby-compilation-executable . "rbx") (ruby-compilation-executable . "jruby") (whitespace-line-column . 80) (lexical-binding . t))))
