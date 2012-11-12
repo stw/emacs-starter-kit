d33c6e7d534f34813e8484c9327dee6d99545cba
Steve Walker <swalker@walkertek.com>
2012-11-11 20:30:07 -0500

    extras

---
+++ steve/color-theme/themes/color-theme-stw-dark.el
@@ -0,0 +1,117 @@
+;;; Author: Ethan Schoonover, Solarized; Greg Pfeil, Emacs adaptation
+;;; URL: http://ethanschoonover.com/solarized
+
+;;; This file is not (YET) part of GNU Emacs.
+
+;;; # Usage
+
+;;; 1. Install the color-theme package
+;;;   (http://www.emacswiki.org/cgi-bin/wiki/ColorTheme)
+;;; 2. Load this file
+;;; 3. M-x color-theme-solarized-[dark|light]
+
+(eval-when-compile
+  (require 'color-theme))
+
+(defun color-theme-stw-dark-theme ()
+  "Color theme by Ethan Schoonover, created 2011-03-24.
+Ported to Emacs by Greg Pfeil, http://ethanschoonover.com/solarized."
+  (let ((base03  "#000304")
+        (base02  "#05232b")
+        (base01  "#586e75")
+        (base00  "#657b83")
+        (base0   "#839496")
+        (base1   "#93a1a1")
+        (base2   "#eee8d5")
+        (base3   "#fdf6e3")
+        (yellow  "#b58900")
+        (orange  "#cb4b16")
+        (red     "#dc322f")
+        (magenta "#d33682")
+        (violet  "#6c71c4")
+        (blue    "#268bd2")
+        (cyan    "#2aa198")
+        (green   "#859900"))
+    (color-theme-install
+     `(color-theme-stw-dark-theme
+       ((foreground-color . ,base0)
+        (background-color . ,base03)
+        (background-mode . "dark")
+        (cursor-color . ,base0))
+       ;; basic
+       (default ((t (:foreground ,base0))))
+       (cursor ((t (:foreground ,base0 :background ,base03 :inverse-video t))))
+       (escape-glyph-face ((t (:foreground ,red))))
+       (fringe ((t (:foreground ,base01 :background ,base02))))
+       (header-line ((t (:foreground ,base0 :background ,base2))))
+       (highlight ((t (:background ,base02))))
+       (isearch ((t (:foreground ,yellow :inverse-video t))))
+       (menu ((t (:foreground ,base0 :background ,base02))))
+       (minibuffer-prompt ((t (:foreground ,blue))))
+       (mode-line
+        ((t (:foreground ,base1 :background ,base02
+                         :box (:line-width 1 :color ,base1)))))
+       (mode-line-buffer-id ((t (:foreground ,base1))))
+       (mode-line-inactive
+        ((t (:foreground ,base0  :background ,base02
+                         :box (:line-width 1 :color ,base02)))))
+       (region ((t (:background ,base02))))
+       (secondary-selection ((t (:background ,base02))))
+       (trailing-whitespace ((t (:foreground ,red :inverse-video t))))
+       (vertical-border ((t (:foreground ,base0))))
+       ;; compilation
+       (compilation-info ((t (:forground ,green :bold t))))
+       (compilation-warning ((t (:foreground ,orange :bold t))))
+       ;; customize
+       (custom-button
+        ((t (:background ,base02 :box (:line-width 2 :style released-button)))))
+       (custom-button-mouse ((t (:inherit custom-button :foreground ,base1))))
+       (custom-button-pressed
+        ((t (:inherit custom-button-mouse
+                      :box (:line-width 2 :style pressed-button)))))
+       (custom-comment-tag ((t (:background ,base02))))
+       (custom-comment-tag ((t (:background ,base02))))
+       (custom-documentation ((t (:inherit default))))
+       (custom-group-tag ((t (:foreground ,orange :bold t))))
+       (custom-link ((t (:foreground ,violet))))
+       (custom-state ((t (:foreground ,green))))
+       (custom-variable-tag ((t (:foreground ,orange :bold t))))
+       ;; diff
+       (diff-added ((t (:foreground ,green :inverse-video t))))
+       (diff-changed ((t (:foreground ,yellow :inverse-video t))))
+       (diff-removed ((t (:foreground ,red :inverse-video t))))
+       ;; emacs-wiki
+       (emacs-wiki-bad-link-face ((t (:foreground ,red :underline t))))
+       (emacs-wiki-link-face ((t (:foreground ,blue :underline t))))
+       (emacs-wiki-verbatim-face ((t (:foreground ,base00 :underline t))))
+       ;; font-lock
+       (font-lock-builtin-face ((t (:foreground ,green))))
+       (font-lock-comment-face ((t (:foreground ,base01))))
+       (font-lock-constant-face ((t (:foreground ,cyan))))
+       (font-lock-function-name-face ((t (:foreground ,blue))))
+       (font-lock-keyword-face ((t (:foreground ,green))))
+       (font-lock-string-face ((t (:foreground ,cyan))))
+       (font-lock-type-face ((t (:foreground ,yellow))))
+       (font-lock-variable-name-face ((t (:foreground ,blue))))
+       (font-lock-warning-face ((t (:foreground ,red :bold t))))
+       ;; info
+       (info-xref ((t (:foreground ,blue :underline t))))
+       (info-xref-visited ((t (:inherit info-xref :foreground ,magenta))))
+       ;; org
+       (org-hide ((t (:foreground ,base03))))
+       (org-todo ((t (:foreground ,red :bold t))))
+       (org-done ((t (:foreground ,green :bold t))))
+       ;; show-paren
+       (show-paren-match-face ((t (:background ,cyan :foreground ,base3))))
+       (show-paren-mismatch-face ((t (:background ,red :foreground ,base3))))))))
+
+(defun color-theme-stw-dark ()
+  (interactive)
+  (color-theme-stw-dark-theme))
+
+(add-to-list 'color-themes
+             '(color-theme-stw-dark
+               "STW Dark"
+               "Stephen Walker <swalker@walkertek.com>"))
+
+(provide 'color-theme-stw-dark)
